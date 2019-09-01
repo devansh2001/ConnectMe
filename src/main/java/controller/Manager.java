ce56f75522596d1509e5cb8dbf5a83bc807843ef
devansh2001 <dponda3@gatech.edu>
2019-08-31 23:10:33 -0400

    Working user validity check

--- src/main/java/controller/Manager.java
+++ src/main/java/controller/Manager.java
@@ -9,10 +9,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import service.ServerService;
 
 import javax.ws.rs.GET;
@@ -31,13 +28,14 @@ public class Manager {
     }
 
     @PutMapping("/send-message")
-    public ResponseEntity sendMessage(MessageToServer messageToServer) {
+    public ResponseEntity sendMessage(@RequestBody MessageToServer messageToServer) {
+        messageToServer.getFrom();
         ServerService serverService = new ServerService();
         System.out.println(serverService);
 //        System.out.println(serverService.getUrl());
 //        System.out.println(serverService.getConnection());
 //        System.out.println();
-//        serverService.processRequest(messageToServer);
+        serverService.processRequest(messageToServer);
         return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 
--- src/main/java/service/ServerService.java
+++ src/main/java/service/ServerService.java
@@ -8,6 +8,8 @@ import java.sql.*;
 
 
 import lombok.extern.java.Log;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Log
 @Slf4j
@@ -21,7 +23,7 @@ public class ServerService {
 
     public ServerService() {
         System.out.println("Hello");
-        this.url = "jdbc:mysql://localhost:3306/ConnectMe?useSSL=false";
+        this.url = "jdbc:mysql://localhost:3306/ConnectMe?serverTimezone=UTC";
         this.user = "root";
         this.password = "adminadmin";
 //        this.url += "user=" + this.user + "&password=" + this.password;
@@ -32,38 +34,45 @@ public class ServerService {
             this.statement = connection.createStatement();
         } catch (SQLException exception) {
             log.info("Connection error while connecting to " + this.url);
+            exception.printStackTrace();
         } catch (Exception exception) {
             log.info("Connection error while connecting to " + this.url + ". " +
                     "ClassNotFound");
         }
     }
 
-    public void processRequest(MessageToServer messageToServer) {
+    public ResponseEntity processRequest(MessageToServer messageToServer) {
         String from = messageToServer.getFrom();
         String to = messageToServer.getTo();
         System.out.println("Connection: " + this.connection);
         System.out.println("Statement: " + this.statement);
         if (checkUserValidity(from) && checkUserValidity(to)) {
-            log.info("Would have written");
+            log.info("Users have been validated");
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            log.info("Users " + from + " and/or " + to + " are invalid ");
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+
         }
-        log.info("Nothing here");
     }
 
     private boolean checkUserValidity(String username) {
         String query =
                 "SELECT * FROM users_list WHERE USERNAME = '" + username + "'";
         try {
-            System.out.println(statement);
-            System.out.println(connection);
-            ResultSet resultSet = statement.executeQuery(query);
-            log.info("Queried to check validity of user: " + username);
-            if (resultSet.wasNull()) {
-                return false;
+            ResultSet rs = statement.executeQuery(query);
+//            System.out.println(resultSet);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                if (username.equals(rs.getString(1))) {
+                    return true;
+                }
             }
         } catch (SQLException exception) {
             log.info("Error while querying users_list for " + username);
             return false;
         }
-        return true;
+        return false;
     }
 }
